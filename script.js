fc9c8d4af6329204c4e4b168eacea9ef56acc634
“Kempt101” <Kempt101@gmail.com>
2022-05-31 15:51:19 -0500

    Create basic functions add, subtract, multiply, divide

---
+++ script.js
@@ -0,0 +1,15 @@
+let addition = function (a, b) {
+  return a + b;
+};
+
+let subtraction = function (a, b) {
+  return a - b;
+};
+
+let multiply = function (a, b) {
+  return a * b;
+};
+
+let divide = function (a, b) {
+  return a / b;
+};
