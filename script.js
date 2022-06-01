f2b39a0c82bf1a8aa2237ca254ac01d545476bb7
“Kempt101” <Kempt101@gmail.com>
2022-06-01 16:04:46 -0500

    Add buttons for calculator

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ let multiply = function (a, b) {
 let divide = function (a, b) {
   return a / b;
 };
+
+let operate = function (operator, a, b) {
+  switch (operator) {
+    case "+":
+      return addition(a, b);
+    case "-":
+      return subtraction(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      return console.log("error: something happened");
+  }
+};
