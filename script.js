f2a955c8bace4a322af2b1e0e1d91f50dc32ebba
“Kempt101” <Kempt101@gmail.com>
2022-06-04 18:41:33 -0500

    Add functions to update display value and convert value to number when number buttons are pressed

--- script.js
+++ script.js
@@ -28,3 +28,14 @@ let operate = function (operator, a, b) {
       return console.log("error: something happened");
   }
 };
+const display = document.getElementById("calcResult");
+let displayValue;
+const calculatorButtons = document.querySelectorAll(".calculatorButtons");
+/* Adds event listener to all the calculator buttons and then updates the display with whatever button is clicked */
+calculatorButtons.forEach((button) => {
+  display.textContent = ""; //Sets the value to a empty string to remove the intial value of the calculator
+  button.addEventListener("click", function updateDisplay() {
+    displayValue = display.textContent += button.textContent; //can't convert here because we need string for display on calculator
+    displayValue = Number(displayValue); //Converts string to number for use in calculator functions
+  });
+});
